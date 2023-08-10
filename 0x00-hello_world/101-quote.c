b2a5be2e69e28a8df04c2cd264633d098335e2c2
Hazim Elhussieny <hazim.elhussieny@yahoo.com>
2023-08-10 11:54:33 -0700

    First day in C Programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,15 +4,12 @@
 
 /*
  * main - entry part of the program
- * 
- * Descritpion : C program that prints exactly
- * ((and that piece of art is useful - Dora Korpar, 2015-10-19,))
- *
+ *  Descritpion : C program that prints exactly
+ *((and that piece of art is useful - Dora Korpar, 2015-10-19,))
  */
 int main(void) 
 {
-char sentence[]= 
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDOUT_FILENO, sentence, strlen(sentence));
+char sntnc[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDOUT_FILENO, sntnc, strlen(sntnc));
 return (1);
 }
