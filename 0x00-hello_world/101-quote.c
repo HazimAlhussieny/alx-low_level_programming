ec88dc8f8dc00d8d6e54d502648aa72d349043ac
Hazim Elhussieny <hazim.elhussieny@yahoo.com>
2023-08-10 11:57:38 -0700

    First day in C Programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,11 @@
 #include <string.h>
 
 /*
- * main - Entry point
- *
- * Return: Always 1 (Success)
- *  
+ * main - Entry point to the program
+ * Return: Always 1 (Success)  
  * Descritpion : C program that prints exactly
  *((and that piece of art is useful - Dora Korpar, 2015-10-19,))
  */
-
 int main(void) 
 {
 char sntnc[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
