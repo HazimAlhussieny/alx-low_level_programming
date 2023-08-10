e1448f9b18cb87ec58a06316739c13fbf2ff3c73
Hazim Elhussieny <hazim.elhussieny@yahoo.com>
2023-08-10 09:12:17 -0700

    First day in C Programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-/*
- * This is entry level to the program
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program demonstrates the use of the printf function
+ * to print a sentence to the standard output.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
-{ 
-	printf("with proper grammar, but the outcome is a piece of art,\n" );
-	return (0);
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
+
