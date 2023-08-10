708855c9c21d44a1e4c9e416cc9a542c24a2e886
Hazim Elhussieny <hazim.elhussieny@yahoo.com>
2023-08-10 12:13:01 -0700

    First day in C Programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,7 @@
 
 /**
 *main - Entry point to the program.
-*Description: C program that prints a specific
-*sentence to the standard output. 
+*Description: C program that prints a sentence. 
 *Return: Always 1 (Success).
 */
 
