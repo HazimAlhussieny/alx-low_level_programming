1d5f3d097ecf73fb908c22feeaa8a83a09cf6274
Hazim Elhussieny <hazim.elhussieny@yahoo.com>
2023-08-10 10:11:30 -0700

    First day in C Programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /*
  * main - Entry point of the program
@@ -8,18 +8,17 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-    char a;
-    int b;
-    long int c;
-    long long int d;
-    float e;
+	char a;
+   	int b;
+    	long int c;
+    	long long int d;
+    	float e;
 
-    printf("Size of a char: %zu byte(s)\n", sizeof(a));
-    printf("Size of an int: %zu byte(s)\n", sizeof(b));
-    printf("Size of a long int: %zu byte(s)\n", sizeof(c));
-    printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
-    printf("Size of a float: %zu byte(s)\n", sizeof(e));
+    	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+    	printf("Size of an int: %zu byte(s)\n", sizeof(b));
+    	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
+    	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
+    	printf("Size of a float: %zu byte(s)\n", sizeof(e));
 
-    return (0);
+    	return (0);
 }
