aa8055c09f455be08060b3f59df13c31652f6477
Hazim Elhussieny <hazim.elhussieny@yahoo.com>
2023-08-11 10:23:45 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - entry point to the program
+*
+* Description: All numbers in base 16 /lowercase 
+*
+* Return: Always (0) success
+*/
+
+int main(void)
+{
+int numbers;
+char alphabet;
+for (numbers = 0 ; number <= 9 ; numbers ++)
+{
+	putchar('0' + numbers);
+}
+for (alphabet = 'a' ; alphabet <= 'f' ; alphabet++)
+{
+	putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
