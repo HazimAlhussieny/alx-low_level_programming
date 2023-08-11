e176b02585c5fe859b5d31872023aae69fb880f2
Hazim Elhussieny <hazim.elhussieny@yahoo.com>
2023-08-11 10:42:50 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main- entry point to the program
+*
+* Description: All possible combinations of single number
+*
+* Return: Always (0) success
+*/
+
+int main(void)
+{
+int numb;
+for (numb = 0 ; numb <= 9 ; numb++)
+{
+	putchar('0', numb);
+	putchar(',');
+	putchar(' ');
+}
+putchar('\n');
+return (0);
+}
