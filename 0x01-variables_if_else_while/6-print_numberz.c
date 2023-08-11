b0e7214ace6839200b95d2ea6139b478c1c78d76
Hazim Elhussieny <hazim.elhussieny@yahoo.com>
2023-08-11 09:52:03 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - entry point to the program 
+*
+* Description: print using putchar
+*
+* Return: Always (0) success
+*
+*/
+
+int main(void)
+{
+int i;
+int j = 0;
+int n_array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+char a_array[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+char letter_number;
+for (i = 0 ; i < 10 ; i++)
+{
+	a_array[i] = n_array[i];
+}
+for (letter_number = a_array[j] ; letter_number <= 'j' ; j++)
+{
+	putchar(letter_number);
+}
+putchar('\n');
+return(0);
+}
