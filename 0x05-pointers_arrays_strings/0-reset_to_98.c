2771ac070f0038868970ec8d533515646417583d
Hazim Elhussieny <hazim.elhussieny@yahoo.com>
2023-08-21 13:12:05 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,13 +3,13 @@
 /**
 *main- entry point to the program
 *
-*Description:  function that takes a pointer to an int as
+*Description: function that takes a pointer to an int as
 *parameter and updates the value it points to to 98.
 *
 *Return: Always (0) success
 */
 
-void reset_to_98(int*n)
+void reset_to_98(int *n)
 {
 *n = 98;
 }
