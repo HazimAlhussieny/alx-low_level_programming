f724b47d44c63492b173419d0104100680c63ca1
Hazim Elhussieny <hazim.elhussieny@yahoo.com>
2023-08-21 15:31:01 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,19 +10,15 @@ void rev_string(char *s)
 {
 	int i, c, k;
 	char *a, aux;
-
 	a = s;
-
 	while (s[c] != '\0')
 	{
 		c++;
 	}
-
 	for (k = 1; k < c; k++)
 	{
 		a++;
 	}
-
 	for (i = 0; i < (c / 2); i++)
 	{
 		aux = s[i];
