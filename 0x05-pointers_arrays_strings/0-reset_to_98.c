9096e1cf7999f879a171f5b3b05286fbe816411f
Hazim Elhussieny <hazim.elhussieny@yahoo.com>
2023-08-21 13:09:21 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*main- entry point to the program
+*
+*Description:  function that takes a pointer to an int as
+*parameter and updates the value it points to to 98.
+*
+*Return: Always (0) success
+*/
+
+void reset_to_98(int * n)
+{
+*n=98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+ 0x01. C - Variables, if, else, while 
+0x05-pointers_arrays_strings
